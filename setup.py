05a1a81d8efc89d80341ef12ed7e09545d6260fc
Patryk Biernat <bearnacki@gmail.com>
2022-05-05 20:12:49 +0200

    setup update

--- setup.py
+++ setup.py
@@ -6,12 +6,9 @@ setup(
     url='https://github.com/bearnacki/python-test-project/',
     license='MIT',
     author='Patryk Biernat',
-    install_requires=['flask==2.1.1',
-                      'Werkzeug==2.1.1',
-                      'pandas==1.4.2',
+    install_requires=['pandas==1.4.2',
                       'openpyxl==3.0.9',
-                      'flasgger==0.9.5'
-                      ],
+                      'flask-restx==0.5.1'],
     author_email='patrbiernat@gmail.com',
     description='Flask API that allow uploading an Excel file and returns a summary of provided columns ',
     packages=['python_test_project']
